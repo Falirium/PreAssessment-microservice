0df82aa87bb9d63156613052ab344e3b2352ce2a
falirium <mafarfaoua@insea.ac.ma>
2022-07-13 10:20:20 +0100

    create the project

---
+++ src/main/java/ma/gbp/assessment/PreAssessmentApplication.java
@@ -0,0 +1,13 @@
+package ma.gbp.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/gbp/assessment/PreAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.gbp.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
